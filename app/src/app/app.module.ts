0cdb110f35cbb3eb0f98bfaeff15ace65f36d065
Christyan Santos <christyanrosa@hotmail.com>
2019-01-04 07:38:43 -0200

    Teste de players de audio

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HomePage } from '../pages/home/home';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {NativeAudio} from "@ionic-native/native-audio";
+import {StreamingMedia} from "@ionic-native/streaming-media";
 
 @NgModule({
   declarations: [
@@ -25,7 +27,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    NativeAudio,
+    StreamingMedia,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
   ]
 })
 export class AppModule {}
--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {NativeAudio} from "@ionic-native/native-audio";
+import {StreamingMedia} from '@ionic-native/streaming-media'
+import {StreamingAudioOptions} from "@ionic-native/streaming-media";
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private onSuccess: any;
+  private onError: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private nativeAudio: NativeAudio, public streamingMedia: StreamingMedia ) {
 
   }
 
+  play() {
+    this.nativeAudio.preloadSimple('naruto', 'src/assets/sounds/narutoSound.mp3').then(this.onSuccess, this.onError)
+    this.nativeAudio.play('naruto',()=>console.log('tururu tocando'))
+
+  }
+  play2(){
+    console.log('dsa')
+    let options: StreamingAudioOptions = {
+      successCallback: () => { console.log('Video played') },
+      errorCallback: (e) => { console.log('Error streaming') },
+    };
+    this.streamingMedia.playAudio('src/assets/sounds/narutoSound.mp3', options)
+  }
 }
