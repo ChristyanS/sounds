8869e8f4c0ebc9c0f49953801ed49f86733a494e
Christyan Santos <christyanrosa@hotmail.com>
2019-01-20 22:18:50 -0200

    Personalização dos icones

---
+++ app/src/models/sound.ts
@@ -0,0 +1,11 @@
+export class Sound {
+  /**
+   *
+   * @param id identificador único
+   * @param path caminho da música
+   * @param icon nome do ícone
+   */
+  constructor(public id: string, public path: string,public icon: string) {
+
+  }
+}
--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -1,15 +1,28 @@
 import {Component} from '@angular/core';
 import {Platform} from 'ionic-angular';
 import {NativeAudio} from "@ionic-native/native-audio";
+import {Sound} from "../../models/sound";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public sounds: Array<Sound> = new Array<Sound>();
   private currentSoundID: string;
 
   constructor(public nativeAudio: NativeAudio, public platform: Platform) {
+    this.sounds.push(new Sound('0', 'assets/sounds/narutoSound.mp3', 'custom-naruto'))
+    this.sounds.push(new Sound('1', 'assets/sounds/xfiles.mp3', 'custom-xfiles'))
+    this.sounds.push(new Sound('2', 'assets/sounds/ayrtonSenna.mp3', 'custom-formula1'))
+    this.sounds.push(new Sound('3', 'assets/sounds/turnDownForWhat.mp3', 'custom-glasses'))
+    this.sounds.push(new Sound('4', 'assets/sounds/pracaNossa.mp3', 'custom-bench'))
+    this.sounds.push(new Sound('5', 'assets/sounds/errou.mp3', 'custom-error'))
+    this.sounds.push(new Sound('6', 'assets/sounds/marchaImperial.mp3', 'custom-darth-vader'))
+    this.sounds.push(new Sound('7', 'assets/sounds/globo.mp3', 'custom-globo'))
+    this.sounds.push(new Sound('8', 'assets/sounds/missaoImposivel.mp3', 'custom-spy'))
+    this.sounds.push(new Sound('9', 'assets/sounds/faroeste.mp3', 'custom-sheriff'))
+
 
   }
 
@@ -36,7 +49,10 @@ export class HomePage {
       console.log('Essa funcionalidade está disponível apenas para dispositivos móveis');
     }
 
-  }cl
+  }
+
+  cl
+
   /**
    * Metodo que descarrega um audio da memoria
    * @param id identificador unico do audio
